9a73a394674f41bef61accd4f87c4b22bf6a43cb
furkanturedi <furkantrd52@gmail.com>
2025-03-06 16:50:38 +0300

    first param düzeltildi

--- first_word/first_word.c
+++ first_word/first_word.c
@@ -4,21 +4,11 @@ int		main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		while (*av[1])
-		{
-			if (*av[1] >= 'A' && *av[1] <= 'Z')
-			{
-				*av[1] += 32;
-				write(1, av[1], 1);
-			}
-			else if (*av[1] >= 'a' && *av[1] <= 'z')
-			{
-				*av[1] -= 32;
-				write(1, av[1], 1);
-			}
-			av[1]++;
-		}
+		while (*av[1] && (*av[1] == ' ' || *av[1] == '\t'))
+			++av[1];
+		while (*av[1] != '\0' && (*av[1] != ' ' && *av[1] != '\t'))
+			write(1, av[1]++, 1);
 	}
-	write(1, "\n", 1);
-	return (1);
+	ft_putchar('\n');
+	return (0);
 }
